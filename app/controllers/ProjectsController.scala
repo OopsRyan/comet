bac821a09dcc15e5c789ef0898b03b9e257dcfb2
Ryan <qirun_chen@163.com>
2018-08-17 23:17:18 +0100

    integrate GapMinder, add all the dependencies, but need to fixed the error of unaccessible data file paths in main.js

--- app/controllers/ProjectsController.scala
+++ app/controllers/ProjectsController.scala
@@ -7,14 +7,11 @@ import play.api.mvc._
 @Singleton
 class ProjectsController @Inject()(projectTemp: views.html.projects, cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(projectTemp())
+  }
+
+  def gapMinder() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.gapminder())
   }
 }
